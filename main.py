3f2c499aac16801fd47f4ba5e0a87b6641561330
Sepehr Sahraian <68083280+sprshr@users.noreply.github.com>
2022-01-07 18:19:50 +0000

    Add spotDoor function

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ spot1 = "Car"
 spot2 = "Goat"
 spot3 = "Goat"
 spots = [spot1, spot2, spot3]
+
+def spotDoor():
+    global door1
+    spotChosen = random.randint(0,2)
+    door1 = spots[spotChosen]
+    spots.pop(spotChosen)
+    global door2
+    spotChosen = random.randint(0,1)
+    door2 = spots[spotChosen]
+    spots.pop(spotChosen)
+    global door3
+    door3 = spots[0]
+    spots.pop(0)
